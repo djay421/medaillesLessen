58467ecc1f922cfe44e3afcbb96459e55e5bc1f2
djay421 <102187676+djay421@users.noreply.github.com>
2025-11-12 10:19:18 +0100

    Update Program.cs

--- MedaillesOpdracht/Program.cs
+++ MedaillesOpdracht/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MedaillesOpdracht
@@ -10,9 +11,16 @@ namespace MedaillesOpdracht
     {
         static void Main(string[] args)
         {
+            
+
             Console.WriteLine("hallo");
+            Console.WriteLine("Wat is je naam?: ");
             string Input = Console.ReadLine();
-            Console.WriteLine(Input);
+            string naam = Input;
+            Console.WriteLine("Wat is leeftijd?: ");
+            int leeftijdInput = Convert.ToInt32(Console.ReadLine());
+            int leeftijd = leeftijdInput;
+            Console.WriteLine("Hoi, jouw naam is " + naam + " en je bent " + leeftijd + " jaar oud");
         }
     }
 }
